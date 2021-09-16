17092ceb38982b90e23ad5034745846a680af6f2
Manuel Retamozo <manuel@retamozo.com.ar>
2021-09-16 20:03:56 -0300

    Cambio de nombre en versión para UAT, examplde enviroment debug false

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Mandarina') }}</title>
+    <title>{{ config('app.name', 'Segmentador - Geoestadística INDEC') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
