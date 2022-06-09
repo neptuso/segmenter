9b89659f72b5143ab39e8f3e14e68dc9d906429a
Hernán Alperin <alpe@alum.mit.edu>
2022-06-09 09:34:13 -0300

    qué con tilde

--- resources/views/grafo/show.blade.php
+++ resources/views/grafo/show.blade.php
@@ -174,7 +174,7 @@
         var newLine = "\r\n"
         var message = "1. Eliminar " + $radio +  " , " +$localidad + " de la tabla radio_localidad ";
         message += newLine;
-        message += "2. Buscar en que esquemas se encuentra el radio " + $radio + " y reportar que se eliminará de todos ellos";
+        message += "2. Buscar en qué esquemas se encuentra el radio " + $radio + " y reportar que se eliminará de todos ellos";
         message += newLine;
         message += "3. borrar el radio de la tabla radio ";
         message += newLine;
